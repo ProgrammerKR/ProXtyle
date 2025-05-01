93a584766c4bcfe8369cb59a16a2c0d91b82e6de
ProgrammerKRgit config --global user.name ProgrammerKR <programmerkr.123@gmail.com>
2025-05-01 06:26:13 +0530

    Initial push for ProXtyle → proxtyle (case correction)

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 /* eslint-env meteor */
 
 Package.describe({
-  name: 'proxstyle', // Your package name
+  name: 'proxtyle', // Your package name
   summary: 'A high-performance CSS framework for building responsive, mobile-first web designs.',
   version: '1.0.0', // Adjust the version as necessary
   git: 'https://github.com/ProgrammerKR/ProXtyle.git' // Your GitHub URL for ProXtyle
@@ -12,7 +12,7 @@ Package.describe({
 Package.onUse(api => {
   api.versionsFrom('METEOR@1.0')
   api.addFiles([
-    'dist/css/proxstyle.css', // Path to your compiled CSS file
-    'dist/js/proxstyle.js' // If you have JavaScript functionalities for ProXtyle
+    'dist/css/proxtyle.css', // Path to your compiled CSS file
+    'dist/js/proxtyle.js' // If you have JavaScript functionalities for ProXtyle
   ], 'client')
 })
